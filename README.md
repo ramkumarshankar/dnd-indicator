2d6997c0e5ab238173bf270f1f657c1612b6e6a9
Ramkumar Shankar <ram.i.am@gmail.com>
2013-08-12 23:32:44 +0700

    first version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 dnd-indicator
 =============
 
-a simple Ubuntu indicator to suppress desktop notifications
+A simple bare-bones Ubuntu indicator to suppress desktop notifications. Simulates a do-not-disturb mode in a rather hack-ish, roundabout way by renaming the services file, and killing the notify-osd process when activated. As a result, it requires root access.
---
+++ dnd.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 Ramkumar Shankar
+#
+# Author: Ramkumar Shankar <ram.i.am@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+import sys
+import os
+import gobject
+import gtk
+import appindicator
+
+userAction = True
+
+class Dnd:
+  
+  def __init__(self):
+    self.ind = appindicator.Indicator ("example-simple-client",
+                                       "/usr/share/icons/dnd-indicator/do-not-disturb-sign.png",
+                                       appindicator.CATEGORY_OTHER)
+    self.ind.set_status (appindicator.STATUS_ACTIVE)
+    self.ind.set_attention_icon ("/usr/share/icons/dnd-indicator/do-not-disturb-sign-busy.png")
+    
+    self.menu_setup()
+    self.ind.set_menu(self.menu)
+    
+  def menu_setup(self):
+    self.menu = gtk.Menu()
+    self.dnd_item = gtk.CheckMenuItem("Do not disturb")
+    #self.dnd_item.set_active(False)
+    self.dnd_item.connect("toggled", self.set_dnd)
+    self.dnd_item.show()
+    self.quit_item = gtk.MenuItem("Quit")
+    self.quit_item.connect("activate", self.quit)
+    self.quit_item.show()
+    self.menu.append(self.dnd_item)
+    self.menu.append(self.quit_item)
+      
+  def main(self):
+    gtk.main()
+
+  def set_dnd(self, widget):
+    if widget.active:
+      bashCommand = "sudo mv /usr/share/dbus-1/services/org.freedesktop.Notifications.service /usr/share/dbus-1/services/org.freedesktop.Notifications.service.disabled"
+      os.system(bashCommand)
+      self.ind.set_status (appindicator.STATUS_ATTENTION)
+    else:
+      bashCommand = "sudo mv /usr/share/dbus-1/services/org.freedesktop.Notifications.service.disabled /usr/share/dbus-1/services/org.freedesktop.Notifications.service"
+      os.system(bashCommand)
+      self.ind.set_status (appindicator.STATUS_ACTIVE)
+    bashCommand = "killall notify-osd"
+    os.system(bashCommand)
+
+  def quit(self, widget):
+    #enable notifications if they are not, before exit
+    if self.dnd_item.get_active() == True:
+      bashCommand = "sudo mv /usr/share/dbus-1/services/org.freedesktop.Notifications.service.disabled /usr/share/dbus-1/services/org.freedesktop.Notifications.service"
+      os.system(bashCommand)
+      bashCommand = "killall notify-osd"
+      os.system(bashCommand)
+    sys.exit(0)
+
+if __name__ == "__main__":
+  indicator = Dnd()
+  indicator.main()
